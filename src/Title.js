62302f8571886ee6f8bd30cc03581efeb22fa892
Luna Muylkens <93606228+LunashaGit@users.noreply.github.com>
2022-03-31 18:10:58 +0200

    Add All COmpleted and active ( need modification )

--- src/Title.js
+++ src/Title.js
@@ -7,24 +7,42 @@ import { useState, useEffect } from "react";
 let LSKEY = 'MyTodoApp';
 const Title = () => {
   let initialTodos = [];
+  
   const [todos, setTodos] = useState(() => {
     initialTodos = JSON.parse(localStorage.getItem(LSKEY + ".todos"));
     return initialTodos || [];
   });
-
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([])
+  
+  const filterHandler = () => { 
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true))
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false))
+        break;
+      default:
+        setFilteredTodos(todos)
+    }
+  }
+  
   function addTodo(todo) {
-    setTodos([...todos, todo]);
+    setTodos([...todos, { id: uuidv4(), todo, completed: false }]);
   }
 
   useEffect(() => {
     window.localStorage.setItem(LSKEY + ".todos", JSON.stringify(todos));
   },[todos]);
-
+  useEffect(() => {
+    filterHandler();
+  }, [todos,status]);
   return (
     <div className="Title">
       <h1>Todos</h1>
       <Bar addTodo={addTodo}/>
-      <Todo setTodos={setTodos} todos={todos} />
+      <Todo setStatus={setStatus} setTodos={setTodos} todos={todos} filteredTodos={filteredTodos}/>
     </div>
   );
 }
--- src/Todo.js
+++ src/Todo.js
@@ -1,16 +1,40 @@
 import logo from './logo.svg';
 import Rename from './Rename.png';
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import './Todo.scss';
 import {v4 as uuidv4} from 'uuid';
+let LSKEY = 'MyTodoApp';
 
 
-const Todo = ({todos, setTodos}) => {
-  const ChangeText = (todo) => {
-    const rename = window.prompt("Please enter a new name", todo.target.textContent);
-    if(rename) {
-      todo.target.textContent = rename;
+const Todo = ({todos, setTodos, setStatus, filteredTodos}) => {
+  /* let ChangeText = (event) => {
+      const rename = window.prompt("Please enter a new name", event.target.textContent);
+      if(rename) {
+        event.target.textContent = rename;
     }
+  } */
+  /* todos.map((todo) => {
+    console.log(todo)
+  }) */
+
+  /* useEffect(() => {
+    window.localStorage.setItem(LSKEY + ".todos", JSON.stringify(todos));
+  },[todos]); */
+  
+  const handleClick = (event) => {
+    console.log(event.target)
+    if(event.target.classList.contains('all')) {
+    event.target.classList.remove('all');
+    event.target.classList.add('active');
+    }else if(event.target.classList.contains('active')) {
+      event.target.classList.remove('active');
+      event.target.classList.add('completed');
+    }
+  }
+  
+  const statusHandler = (e) => {
+    console.log(e.target.value);
+    setStatus(e.target.value)
   }
 
   return (
@@ -23,17 +47,24 @@ const Todo = ({todos, setTodos}) => {
           }
         }}>Clean the list</button>
         <ul className="checkbox">
-          {todos.map((todo) => (
-            <li key={uuidv4()}>
+          {filteredTodos.map(todo => (
+            <li className='all' key={uuidv4()}>
               <input name="checkbox" type="checkbox" /> 
-              <label onClick={ChangeText} htmlFor="checkbox">
-               {todo}
+              <label  htmlFor="checkbox">
+               {todo.todo}
               </label>
               
             </li>
           ))}
         </ul>
-        <p>{todos.length} items left</p>
+          <p>{todos.length} items left</p>
+        <div className="footer">
+          <select onChange={statusHandler} className="filter-todo">
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="uncompleted">Uncompleted</option>
+          </select>
+        </div>
       </div>
   );
 }
